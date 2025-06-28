e9416436aae7a3c0ff369fe41fa1845c73444dac
Khushigupta-23 <khushi75gupta8811@gmail.com>
2025-06-28 16:53:24 +0530

    added autheantication

--- routes/user.js
+++ routes/user.js
@@ -2,6 +2,7 @@ const express = require("express");
 const router = express.Router();
 const User = require("../models/user.js");
 const passport = require("passport");
+const { saveRedirectUrl } = require("../middleware.js");
 
 // Signup
 router.get("/signup", (req, res) => {
@@ -35,13 +36,14 @@ router.get("/login", (req, res) => {
 })
 
 router.post("/login",
+    saveRedirectUrl ,
     passport.authenticate('local', {
         failureRedirect: '/login',
         failureFlash: true
     }),
     async (req, res) => {
         req.flash("sucess", "Welcome to Wanderlust ! You are logged in!");
-        res.redirect("/listing");
+        res.redirect(req.session.redirectUrl);
     }
 );
 
